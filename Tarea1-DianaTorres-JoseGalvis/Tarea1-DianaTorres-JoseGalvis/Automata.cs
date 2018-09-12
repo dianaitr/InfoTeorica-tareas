7a41c439275352a328c8ee3748f7c02b0c778a60
diana <diana@PC-DIANAITR>
2018-09-12 14:56:53 -0500

    Agrego clases y metodos getters and setters y los que se necesitaran para la particion.

---
+++ Tarea1-DianaTorres-JoseGalvis/Tarea1-DianaTorres-JoseGalvis/Automata.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1_DianaTorres_JoseGalvis
+{
+    class Automata
+    {
+        private string tipo;
+
+        private List<Estado> estados;
+        private List<Transicion> transiciones;
+
+        private Automata automataMinimo;
+
+        //getters & setters
+        internal List<Estado> Estados { get => estados; set => estados = value; }
+        internal List<Transicion> Transiciones { get => transiciones; set => transiciones = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public Automata AutomataMinimo { get => automataMinimo; set => automataMinimo = value; }
+
+
+        //constructor
+        public Automata(string tipo)
+        {
+            this.Tipo = tipo;
+            estados = new List<Estado>();
+            transiciones = new List<Transicion>();
+            
+        }
+
+
+        //jose
+        public Boolean EstadoMealy(string valor)
+        {
+            return false;
+        }
+
+        //jose
+        public Boolean EstadoMoore(string valor, string respuesta)
+        {
+            return false;
+        }
+
+        //diana
+        public Boolean agregarTransicion(string tipo, string estimulo, string respuesta, string llegada, string salida)
+        {
+            return false;
+        }
+
+        //dianayjose
+        public Automata generarAutomataMinimo()
+        {
+            AutomataMinimo = null;
+            return automataMinimo;
+        }
+    }
+}
---
+++ Tarea1-DianaTorres-JoseGalvis/Tarea1-DianaTorres-JoseGalvis/Estado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1_DianaTorres_JoseGalvis
+{
+    class Estado
+    {
+        private string valor;
+        private string respuestaMoore;
+
+        public Estado(string valor, string respuestaMoore)
+        {
+            this.Valor = valor;
+            this.RespuestaMoore = respuestaMoore;
+        }
+
+        public string Valor { get => valor; set => valor = value; }
+        public string RespuestaMoore { get => respuestaMoore; set => respuestaMoore = value; }
+    }
+}
