93aaf43cf5257d95d704eb973574f8f880619b6f
jose alejandro galvis nieto <jagn1997@hotmail.com>
2018-09-11 19:47:31 -0500

    BOTONN

--- Tarea1-DianaTorres-JoseGalvis/Tarea1-DianaTorres-JoseGalvis/Form1.Designer.cs
+++ Tarea1-DianaTorres-JoseGalvis/Tarea1-DianaTorres-JoseGalvis/Form1.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(419, 119);
+            this.checkBox1.Location = new System.Drawing.Point(314, 97);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 446);
+            this.ClientSize = new System.Drawing.Size(544, 362);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -58,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
